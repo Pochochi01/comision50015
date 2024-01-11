c5863a285073c975aee9c9799340458407272e9e
Mauro Largo <largomaurondres@hotmail.comclear>
2024-01-10 22:53:32 -0300

    modificaciones primera pre entrega

--- src/app.js
+++ src/app.js
@@ -1,54 +1,13 @@
-const express = require("express");
+import express from "express";
 const PORT = 8080;
-const ProductManager = require("./product-manager.js");
-const manager = new ProductManager("./src/products.json");
-
+import productsRouter from "./routes/products.router.js"
+import cartsRouter from "./routes/carts.router.js"
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
-const readProducts = manager.readProduct();
-
-app.get("/",  (req, res) => {
-  res.send("");
-})
-
-
-app.get("/products", async (req, res) => {
- try {
-  let limit = parseInt(req.query.limit);
-  const arrayProductos = await manager.readProduct();
-  if (limit >= 0 && limit < arrayProductos.length){
-    const result = arrayProductos.slice(0,limit);
-    return res.send(result);
-  } else {
-   // return res.send({error: "limite ingresado fuera de rango del arreglo"});
-    return res.send(arrayProductos);
-  }
-
-  }  catch (error){
-     console.log(error);
-     res.send("Error al procesar la solicitud");
-   }
-});
-
-
-app.get("/products/:pid", async (req, res) => {
-  try {
-    let pid = parseInt(req.params.pid);
-    const productById = await manager.getProductById(pid);
-    if (productById) {
-      return res.send(productById);
-    } else {
-      return res.send(productById);
-    }
-  }
-  catch (error) {
-    console.log(error);
-    res.send("Error al Cargar");
-  }
-
-});
+app.use(express.json());
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.listen(PORT, () => {
   console.log(`Escuchando en http://localhost:${PORT}`);
