762a1cd7d31861821dcb4a76a32112217a71170e
Mauro Largo <largomaurondres@hotmail.comclear>
2024-01-17 23:51:17 -0300

    websocket

--- src/app.js
+++ src/app.js
@@ -1,14 +1,50 @@
 import express from "express";
-const PORT = 8080;
-import productsRouter from "./routes/products.router.js"
-import cartsRouter from "./routes/carts.router.js"
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
+import hdsRouter from "./routes/handlebars.router.js"
+import exphbs from "express-handlebars";
+import __dirname from "./utils.js";
+import * as path from "path";
+import Server from "socket.io";
+import ProductManager from "./controllers/product-manager.js";
 
 const app = express();
+const PORT = 8080;
+const productManager = new ProductManager("./src/models/products.json");
+const io = new Server(httpServer);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", express.static(__dirname + "/public"));
+
+
+app.engine("handlebars",exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname + "/views"));
+
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/api/hds", hdsRouter);
 
-app.listen(PORT, () => {
+io.on("connection", async(socket) =>{
+  console.log("Cliente Conectado")
+  socket.emit("productos", await productManager.getProducts());
+  
+  socket.on("elimiarProducto", async (id)=>{
+    await productManager.deleteProductById(id);
+    io.socket.emit("productos", await productManager.getProducts());
+  })
+
+  socket.on("agregarProducto", async (producto)=>{
+    await productManager.addProduct(producto);
+    io.socket.emit("productos", await productManager.getProducts());
+  })
+
+});
+
+
+
+const server = app.listen(PORT, () => {
   console.log(`Escuchando en http://localhost:${PORT}`);
 })
